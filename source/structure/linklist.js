bca45c43deaed8c3fe1528d02ab05dce6a5960de
xiaoxiangdaiyu <935829048@qq.com>
2019-11-05 20:36:26 +0800

    删除节点

--- source/structure/linklist.js
+++ source/structure/linklist.js
@@ -24,3 +24,33 @@ function printVlas(){
         head = next
     }
 }
+
+// 删除链表节点，删除分为几种情况 一般是给定头部节点
+/** 
+ * 首节点
+ * 中间节点
+ * 尾节点
+ */
+function remove(head,node){
+    // 头节点
+    if (node.next) {
+        // 中间节点,将next节点覆盖当前节点
+        node.val = node.next.val;
+        node.next = node.next.next;
+    } else if (head.value = node.value) {
+        // 节点置为null，头部节点也为null,该节点被null覆盖
+        node = null;
+        head = null;
+    }else{
+        // 尾部节点，且非头，则需要遍历到最后一个，next指向null
+        // 从头遍历
+        node = head;
+        // 最后一个之前，当前节点都被next覆盖。
+        while (node.next.next) {
+            node = node.next;
+        }
+        // 没有next指向，即删除尾节点
+        node.next = null;
+        node = null;
+    }
+}
